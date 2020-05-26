821433f0315c3563e8565e3814563083877d78d6
leungmanhin <manhinleung@gmail.com>
2020-05-26 17:09:47 +0800

    Write the atomese to a file

--- analysis.py
+++ analysis.py
@@ -11,21 +11,25 @@ target_id_prefix = "target-"
 feature_prefix = "feature-"
 
 def evalink(pred, node1, node2):
-  return "\n".join(["(Evaluation",
-                    "\t(Predicate \"" + pred + "\")",
-                    "\t(List",
-                    "\t\t(Concept \"" + node1 + "\")",
-                    "\t\t(Concept \"" + node2 + "\")))"])
+  return "\n".join(["(EvaluationLink",
+                    "\t(PredicateNode \"" + pred + "\")",
+                    "\t(ListLink",
+                    "\t\t(ConceptNode \"" + node1 + "\")",
+                    "\t\t(ConceptNode \"" + node2 + "\")))\n"])
 
 if not os.path.isfile(mooc_all_scm):
+  mooc_all_scm_fp = open(mooc_all_scm, "w")
+
   with open(mooc_actions_tsv) as f:
     for line in f:
       content = line.split("\t")
       action_id = content[0].strip()
       user_id = content[1].strip()
       target_id = content[2].strip()
-      print(evalink("takes", user_id_prefix + user_id, action_id_prefix + action_id))
-      print(evalink("has_target", action_id_prefix + action_id, target_id_prefix + target_id))
+      atomese_1 = evalink("takes", user_id_prefix + user_id, action_id_prefix + action_id)
+      atomese_2 = evalink("has_target", action_id_prefix + action_id, target_id_prefix + target_id)
+      mooc_all_scm_fp.write(atomese_1)
+      mooc_all_scm_fp.write(atomese_2)
 
   with open(mooc_action_labels_tsv) as f:
     for line in f:
@@ -33,16 +37,17 @@ if not os.path.isfile(mooc_all_scm):
       action_id = content[0].strip()
       label = content[1].strip()
       if label == "1":
-        print(evalink("leads_to", action_id_prefix + action_id, "dropout"))
+        atomese = evalink("leads_to", action_id_prefix + action_id, "dropout")
+        mooc_all_scm_fp.write(atomese)
 
   features = []
   with open(mooc_action_features_tsv) as f:
     def process_feature(action_id, feature):
       if feature not in features:
         features.append(feature)
-      print(evalink("has_feature",
-                    action_id_prefix + action_id,
-                    feature_prefix + str(features.index(feature))))
+      return evalink("has_feature",
+                     action_id_prefix + action_id,
+                     feature_prefix + str(features.index(feature)))
     for line in f:
       content = line.split("\t")
       action_id = content[0].strip()
@@ -50,7 +55,9 @@ if not os.path.isfile(mooc_all_scm):
       feature_1 = content[2].strip()
       feature_2 = content[3].strip()
       feature_3 = content[4].strip()
-      process_feature(action_id, feature_0)
-      process_feature(action_id, feature_1)
-      process_feature(action_id, feature_2)
-      process_feature(action_id, feature_3)
+      atomese_1 = process_feature(action_id, feature_0)
+      atomese_2 = process_feature(action_id, feature_1)
+      atomese_3 = process_feature(action_id, feature_2)
+      atomese_4 = process_feature(action_id, feature_3)
+
+  mooc_all_scm_fp.close()
